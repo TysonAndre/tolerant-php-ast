6b582e6ae6dee134a488ee7fe88dffc765952de0
Tyson Andre <tysonandre775@hotmail.com>
2022-09-22 08:25:13 -0400

    Document reason for short_open_tag=0

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -4,6 +4,7 @@
 # -x Exit immediately if any command fails
 # -e Echo all commands being executed.
 set -xe
+# The way token_get_all lexes content changes based on the short_open_tag ini setting
 php -d short_open_tag=0 ./vendor/bin/phpunit --testsuite invariants
 php -d short_open_tag=0 ./vendor/bin/phpunit --testsuite grammar
 php -d short_open_tag=0 ./vendor/bin/phpunit --testsuite api
